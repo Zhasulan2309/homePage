79fca987244d0615cc163fb7574a52e73518199a
Zhakkozhayev <zhasulan2309@gmail.com>
2026-01-15 21:34:38 +0500

    Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,49 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const repoName = "homePage";
-
 module.exports = (env, argv) => {
-  const path = require("path");
-  const HtmlWebpackPlugin = require("html-webpack-plugin");
+  const isProd = argv.mode === "production";
 
-  const repoName = "homePage";
-
-  module.exports = (env, argv) => {
-    const isProd = argv.mode === "production";
-
-    const publicPath =
-      process.env.PUBLIC_PATH ?? (isProd ? `/${repoName}/` : "/");
-
-    return {
-      entry: path.resolve(__dirname, "src", "index.jsx"),
-      output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: isProd ? "js/[name].[contenthash:8].js" : "js/bundle.js",
-        clean: true,
-        publicPath,
-      },
-      resolve: {
-        extensions: [".js", ".jsx"],
-        alias: { "@": path.resolve(__dirname, "src") },
-      },
-      module: {
-        rules: [
-          { test: /\.(js|jsx)$/, exclude: /node_modules/, use: "babel-loader" },
-          { test: /\.css$/, use: ["style-loader", "css-loader"] },
-          {
-            test: /\.(png|jpg|jpeg|gif|svg|webp)$/i,
-            type: "asset/resource",
-            generator: { filename: "assets/[name].[hash:8][ext]" },
-          },
-          {
-            test: /\.(woff2?|eot|ttf|otf)$/i,
-            type: "asset/resource",
-            generator: { filename: "fonts/[name].[hash:8][ext]" },
-          },
-        ],
-      },
-      plugins: [
-        new HtmlWebpackPlugin({
-          template: path.resolve(__dirname, "public", "index.html"),
-        }),
+  return {
+    entry: path.resolve(__dirname, "src", "index.jsx"),
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: isProd ? "js/[name].[contenthash:8].js" : "js/bundle.js",
+      clean: true,
+      publicPath: "/", // ✅ только корень домена
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+      alias: { "@": path.resolve(__dirname, "src") },
+    },
+    module: {
+      rules: [
+        { test: /\.(js|jsx)$/, exclude: /node_modules/, use: "babel-loader" },
+        { test: /\.css$/, use: ["style-loader", "css-loader"] },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg|webp)$/i,
+          type: "asset/resource",
+          generator: { filename: "assets/[name].[hash:8][ext]" },
+        },
+        {
+          test: /\.(woff2?|eot|ttf|otf)$/i,
+          type: "asset/resource",
+          generator: { filename: "fonts/[name].[hash:8][ext]" },
+        },
       ],
-      devtool: isProd ? "source-map" : "eval-source-map",
-      devServer: {
-        static: { directory: path.resolve(__dirname, "public") },
-        port: 3000,
-        hot: true,
-        historyApiFallback: true,
-        open: true,
-      },
-    };
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+      }),
+    ],
+    devtool: isProd ? "source-map" : "eval-source-map",
+    devServer: {
+      static: { directory: path.resolve(__dirname, "public") },
+      port: 3000,
+      hot: true,
+      historyApiFallback: true,
+      open: true,
+    },
   };
 };
